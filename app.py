68efe2ebbdac5cfab78a41c2dc1796ec569181c3
Rizkyy3 <m.rizkyf400@gmail.com>
2025-05-06 08:59:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,25 +14,3 @@ if __name__ == "__main__":
 
     cmd = input("Enter a command to run: ")
     run_command(cmd)
-import subprocess
-
-def greet(name):
-    print(f"Hello, {name}!")
-
-def run_command(cmd):
-<<<<<<< HEAD
-    subprocess.call(cmd, shell=True)  # VULNERABILITY: command injection
-=======
-    subprocess.call(cmd, shell=True)
->>>>>>> 342612c (Initial commit with vulnerable application)
-
-if __name__ == "__main__":
-    name = input("Enter your name: ")
-    greet(name)
-
-    cmd = input("Enter a command to run: ")
-    run_command(cmd)
-<<<<<<< HEAD
-=======
-
->>>>>>> 342612c (Initial commit with vulnerable application)
